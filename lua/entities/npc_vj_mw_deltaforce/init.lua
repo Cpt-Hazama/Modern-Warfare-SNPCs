439b3f4c180ed39f9dd74da5159e964ca6e0d120
Cpt-Hazama <Caleb123459@gmail.com>
2022-05-24 02:23:18 -0400

    Icons

--- lua/entities/npc_vj_mw_deltaforce/init.lua
+++ lua/entities/npc_vj_mw_deltaforce/init.lua
@@ -5,7 +5,16 @@ include('shared.lua')
 	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
 	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
 -----------------------------------------------*/
-ENT.Model = {"models/tfusion/playermodels/mw3/mp_delta_elite_a.mdl"}
+ENT.Model = {
+	"models/tfusion/playermodels/mw3/mp_delta_elite_a.mdl",
+	"models/tfusion/playermodels/mw3/mp_delta_elite_b.mdl",
+	"models/tfusion/playermodels/mw3/mp_delta_elite_c.mdl",
+	"models/tfusion/playermodels/mw3/mp_delta_elite_d.mdl"
+}
 
 ENT.VJ_NPC_Class = {"CLASS_PLAYER_ALLY"}
 ENT.FriendsWithAllPlayerAllies = true
+---------------------------------------------------------------------------------------------------------------------------------------------
+function ENT:OnInit()
+	self:SetLoadout(math.random(1,5))
+end
